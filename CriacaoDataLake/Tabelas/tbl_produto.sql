4f51568c1366bd063cda2ae74b25489d69d11fed
RicardoMelogit <ricardoomelo1883@gmail.com>
2026-01-18 20:11:54 -0300

    Adicionar arquivos de projeto.

---
+++ CriacaoDataLake/Tabelas/tbl_produto.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[tbl_produto]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [cod_produto] VARCHAR(25) NULL, 
+    [desc_produto] VARCHAR(200) NULL, 
+    [atr_unidade_medida] VARCHAR(50) NULL, 
+    [cod_fornecedor] VARCHAR(25) NULL, 
+    [desc_fornecedor] VARCHAR(200) NULL, 
+    [arquivo_origem] VARCHAR(200) NULL, 
+    [data_carga] DATETIME NULL
+)
